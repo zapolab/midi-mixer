dc18645cd50420f27b1b2ca20665694bf6a7a797
tisatoelia <devele14@gmail.com>
2026-06-28 19:29:24 +0200

    feat: crates import

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+use core::fmt::Write;
+use defmt::{info, warn};
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
-use embassy_time::Timer;
+use embassy_rp::{
+    adc::{Adc, Channel, Config as AdcConfig, InterruptHandler as AdcInterruptHandler},
+    bind_interrupts,
+    gpio::{self, Pull},
+    i2c::{Async, Config as I2cConfig, I2c, InterruptHandler as I2cInterruptHandler},
+    peripherals::I2C0,
+};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_time::{Duration, Timer};
+use embedded_graphics::{
+    mono_font::{MonoTextStyleBuilder, ascii::FONT_6X10},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::Text,
+};
 use gpio::{Level, Output};
+use heapless::String;
+use ssd1306::{
+    I2CDisplayInterface, Ssd1306, prelude::*, rotation::DisplayRotation, size::DisplaySize128x64,
+};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
